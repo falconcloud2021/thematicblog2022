be17d56730495c42fc2d0e8a65b4207b6591beb1
falconcloud2021 <bondarchuck.yaroslaw@gmail.com>
2022-01-21 16:44:39 +0200

    added and fix views posts/dashboard pages, admin layouts

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -38,6 +38,66 @@
               </div>
               <!-- Card -->
               <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+                <div class="p-3 mr-4 text-green-500 bg-green-100 rounded-full dark:text-green-100 dark:bg-green-500">
+                  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                    <path fill-rule="evenodd"
+                      d="M4 4a2 2 0 00-2 2v4a2 2 0 002 2V6h10a2 2 0 00-2-2H4zm2 6a2 2 0 012-2h8a2 2 0 012 2v4a2 2 0 01-2 2H8a2 2 0 01-2-2v-4zm6 4a2 2 0 100-4 2 2 0 000 4z"
+                      clip-rule="evenodd">
+                    </path>
+                  </svg>
+                </div>
+                <div>
+                  <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400"> Account balance </p>
+                  <p class="text-lg font-semibold text-gray-700 dark:text-gray-200"> $ 46,760.89 </p>
+                </div>
+              </div>
+              <!-- Card -->
+              <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+                <div class="p-3 mr-4 text-green-500 bg-green-100 rounded-full dark:text-green-100 dark:bg-green-500">
+                  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                    <path fill-rule="evenodd"
+                      d="M4 4a2 2 0 00-2 2v4a2 2 0 002 2V6h10a2 2 0 00-2-2H4zm2 6a2 2 0 012-2h8a2 2 0 012 2v4a2 2 0 01-2 2H8a2 2 0 01-2-2v-4zm6 4a2 2 0 100-4 2 2 0 000 4z"
+                      clip-rule="evenodd">
+                    </path>
+                  </svg>
+                </div>
+                <div>
+                  <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400"> Account balance </p>
+                  <p class="text-lg font-semibold text-gray-700 dark:text-gray-200"> $ 46,760.89 </p>
+                </div>
+              </div>
+              <!-- Card -->
+              <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+                <div class="p-3 mr-4 text-green-500 bg-green-100 rounded-full dark:text-green-100 dark:bg-green-500">
+                  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                    <path fill-rule="evenodd"
+                      d="M4 4a2 2 0 00-2 2v4a2 2 0 002 2V6h10a2 2 0 00-2-2H4zm2 6a2 2 0 012-2h8a2 2 0 012 2v4a2 2 0 01-2 2H8a2 2 0 01-2-2v-4zm6 4a2 2 0 100-4 2 2 0 000 4z"
+                      clip-rule="evenodd">
+                    </path>
+                  </svg>
+                </div>
+                <div>
+                  <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400"> Account balance </p>
+                  <p class="text-lg font-semibold text-gray-700 dark:text-gray-200"> $ 46,760.89 </p>
+                </div>
+              </div>
+              <!-- Card -->
+              <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+                <div class="p-3 mr-4 text-green-500 bg-green-100 rounded-full dark:text-green-100 dark:bg-green-500">
+                  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                    <path fill-rule="evenodd"
+                      d="M4 4a2 2 0 00-2 2v4a2 2 0 002 2V6h10a2 2 0 00-2-2H4zm2 6a2 2 0 012-2h8a2 2 0 012 2v4a2 2 0 01-2 2H8a2 2 0 01-2-2v-4zm6 4a2 2 0 100-4 2 2 0 000 4z"
+                      clip-rule="evenodd">
+                    </path>
+                  </svg>
+                </div>
+                <div>
+                  <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400"> Account balance </p>
+                  <p class="text-lg font-semibold text-gray-700 dark:text-gray-200"> $ 46,760.89 </p>
+                </div>
+              </div>
+              <!-- Card -->
+              <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
                 <div class="p-3 mr-4 text-blue-500 bg-blue-100 rounded-full dark:text-blue-100 dark:bg-blue-500">
                   <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                     <path
@@ -66,14 +126,46 @@
                 </div>
               </div>
             </div>
-            <!-- Charts -->
-            <h6 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200"> Charts </h6>
+            <!-- Reports charts -->
             <div class="grid gap-6 mb-8 md:grid-cols-2">
+              <!-- Bars chart -->
+              <div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+                <h4 class="mb-4 font-semibold text-gray-800 dark:text-gray-300"> Bars </h4>
+                <canvas id="bars"></canvas>
+                <div class="flex justify-center mt-4 space-x-3 text-sm text-gray-600 dark:text-gray-400">
+                  <!-- bars chart legend -->
+                  <div class="flex items-center">
+                    <span class="inline-block w-3 h-3 mr-1 bg-teal-500 rounded-full"></span>
+                    <span>Shoes</span>
+                  </div>
+                  <div class="flex items-center">
+                    <span class="inline-block w-3 h-3 mr-1 bg-purple-600 rounded-full"></span>
+                    <span>Bags</span>
+                  </div>
+                </div>
+              </div>
+              <!-- Lines chart -->
+              <div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+                <h4 class="mb-4 font-semibold text-gray-800 dark:text-gray-300"> Traffic </h4>
+                <canvas id="line"></canvas>
+                <div class="flex justify-center mt-4 space-x-3 text-sm text-gray-600 dark:text-gray-400">
+                  <!-- lines chart legend -->
+                  <div class="flex items-center">
+                    <span class="inline-block w-3 h-3 mr-1 bg-teal-600 rounded-full"></span>
+                    <span> Organic </span>
+                  </div>
+                  <div class="flex items-center">
+                    <span class="inline-block w-3 h-3 mr-1 bg-purple-600 rounded-full"></span>
+                    <span> Paid </span>
+                  </div>
+                </div>
+              </div>
+              <!-- Revenue chart -->
               <div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
                 <h4 class="mb-4 font-semibold text-gray-800 dark:text-gray-300"> Revenue </h4>
                 <canvas id="pie"></canvas>
                 <div class="flex justify-center mt-4 space-x-3 text-sm text-gray-600 dark:text-gray-400">
-                  <!-- Chart legend -->
+                  <!-- revenue chart legend -->
                   <div class="flex items-center">
                     <span class="inline-block w-3 h-3 mr-1 bg-blue-500 rounded-full"></span>
                     <span> Shirts </span>
@@ -88,18 +180,23 @@
                   </div>
                 </div>
               </div>
+              <!-- Free chart -->
               <div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
-                <h4 class="mb-4 font-semibold text-gray-800 dark:text-gray-300"> Traffic </h4>
-                <canvas id="line"></canvas>
+                <h4 class="mb-4 font-semibold text-gray-800 dark:text-gray-300"> Revenue </h4>
+                <canvas id="pie"></canvas>
                 <div class="flex justify-center mt-4 space-x-3 text-sm text-gray-600 dark:text-gray-400">
-                  <!-- Chart legend -->
+                  <!-- free chart legend -->
+                  <div class="flex items-center">
+                    <span class="inline-block w-3 h-3 mr-1 bg-blue-500 rounded-full"></span>
+                    <span> Shirts </span>
+                  </div>
                   <div class="flex items-center">
                     <span class="inline-block w-3 h-3 mr-1 bg-teal-600 rounded-full"></span>
-                    <span> Organic </span>
+                    <span> Shoes </span>
                   </div>
                   <div class="flex items-center">
                     <span class="inline-block w-3 h-3 mr-1 bg-purple-600 rounded-full"></span>
-                    <span> Paid </span>
+                    <span> Bags </span>
                   </div>
                 </div>
               </div>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -12,6 +12,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" defer></script>
     <script src="/admin/assets/js/charts-lines.js" defer></script>
     <script src="/admin/assets/js/charts-pie.js" defer></script>
+    <script src="/admin/assets/js/charts-bars.js" defer></script>
   </head>
   <body>
     <div class="flex h-screen bg-gray-50 dark:bg-gray-900" :class="{ 'overflow-hidden': isSideMenuOpen }">
